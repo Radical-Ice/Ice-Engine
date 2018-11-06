31d4e32026b3eb1fd8179e3993204ca44412272c
Jatin Kumar <jatin.kumarroxx@hotmail.com>
2018-11-06 12:12:34 -0500

    Some changes

--- Ice Engine/Lib/Game Window.h
+++ Ice Engine/Lib/Game Window.h
@@ -18,30 +18,25 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	PAINTSTRUCT ps;
 	HDC hdc;
 	TCHAR greeting[] = _T("Hello, World!");
-	TCHAR Action[] = _T("Pressed ");
+	TCHAR Action[] = _T("Pressed : ");
 	TCHAR Button[] = _T("Press Here");
 	POINT pt;
 	static POINTS ptsBegin;
-	LPRECT rect = new RECT{ 25,20,100,100 };
+	LPRECT rect = new RECT{ 70,30,100,100 };
 	LPRECT rect2 = new RECT{ 50,50,150,100 };
 
 	switch (message)
 	{
-
-	case WM_CHAR:
+	case WM_LBUTTONDOWN:
+	case WM_RBUTTONDOWN:
+	case WM_KEYDOWN:
 		//if (wParam == 97) {	
 			myBool = true;
 			value = (LPCSTR)&wParam;
 			storedKey = (string)value;
 			toPrint = &storedKey[0];
 			InvalidateRect(hWnd, rect, 1);
-		//	PostMessage(hWnd, WM_PAINT, 0, 0);
-			//MessageBox(hWnd, (LPCTSTR)"You just pushed me!", (LPCTSTR) "My Program!", MB_OK | MB_ICONEXCLAMATION);
-			
-
-		//}
-		break;
-	case WM_KEYDOWN:
+			cout << toPrint << endl;
 		break;
 	case WM_PAINT :
 		hdc = BeginPaint(hWnd, &ps);
@@ -52,6 +47,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 		//Printing Greetings
 		TextOut(hdc,5, 5,greeting, _tcslen(greeting));
+		TextOut(hdc, 0, 30, Action, _tcslen(Action));
 
 		if (myBool) {
 			SetTextColor(hdc, RGB(255, 0, 0));
@@ -63,16 +59,21 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		ptsBegin = MAKEPOINTS(lParam);
 		break;
 
-	case WM_LBUTTONDOWN:
-		pt.x = ptsBegin.x;
-		pt.y = ptsBegin.y;
-		if (PtInRect(rect2, pt))
-		{
-			cout << "Pressed" << endl;
-			myBool = true;
-			InvalidateRect(hWnd, rect, 1);
-		}
-		break;
+	//case WM_LBUTTONDOWN:
+	//	/*pt.x = ptsBegin.x;
+	//	pt.y = ptsBegin.y;
+	//	if (PtInRect(rect2, pt))
+	//	{
+	//		cout << "Pressed" << endl;
+	//		myBool = true;
+	//		InvalidateRect(hWnd, rect, 1);
+	//	}*/
+	//	myBool = true;
+	//	value = (LPCSTR)&wParam;
+	//	storedKey = (string)value;
+	//	toPrint = &storedKey[0];
+	//	InvalidateRect(hWnd, rect, 1);
+	//	break;
 
 	case WM_DESTROY:
 		PostQuitMessage(0);
